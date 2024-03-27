d98f29ec1773e707ad28b710b01c8e4936927254
Ian Rossiter <ian.rossiter@madepeople.se>
2024-03-27 20:10:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('volunteer-btn').addEventListener('click', function() {
+        // Here you might open a new page or a form for volunteers to sign up.
+        window.location.href='https://twitter.com/safetyNetSwe';
+    });
+});
